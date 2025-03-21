e80661d867e172bc9758c8a4da111ee3620ee5a3
CannisGU <2229362337@qq.com>
2025-03-21 12:45:43 +0800

    Update theme_extra.js

--- js/theme_extra.js
+++ js/theme_extra.js
@@ -4,5 +4,7 @@
  *
  * https://github.com/mkdocs/mkdocs/issues/2028
  */
-
+// 初始化主题
+const savedTheme = localStorage.getItem('theme') || 'light';
+document.documentElement.classList.toggle('dark-theme', savedTheme === 'dark');
 $('div.rst-content table').addClass('docutils');
